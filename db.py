273b0c7840a7b85f6fa8afe8aa4c6ba5322eda43
Huntik <FDyb8A8V>
2023-04-17 18:20:10 +0500

    creating a nickname function, and checking the registration stage and changing it

--- db.py
+++ db.py
@@ -14,3 +14,18 @@ class Database:
         with self.connection:
             result = self.cursor.execute("SELECT * FROM `users` WHERE `user_id` = ?", (user_id,)).fetchall()
             return bool(len(result))
+
+    def set_nickname(self, user_id, nickname):
+        with self.connection:
+            return self.cursor.execute("UPDATE `users` SET `nickname` = ? WHERE `user_id` = ?", (nickname, user_id,))
+
+    def get_signup(self, user_id):
+        with self.connection:
+            result = self.cursor.execute("SELECT `signup` FROM `users` WHERE `user_id` = ?", (user_id,)).fetchall()
+            for row in result:
+                signup = str(row[0])
+            return signup
+
+    def set_signup(self, user_id, signup):
+        with self.connection:
+            return self.cursor.execute("UPDATE `users` SET `signup` = ? WHERE user_id` = ?", (signup, user_id,))
